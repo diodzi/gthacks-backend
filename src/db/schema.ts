c7d653d86e144da3193efbd2195e96d0b3c1dd09
Marion Office <87404335+Marioffic@users.noreply.github.com>
2025-09-27 03:59:34 -0400

    added betsTable shape

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
-import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
+import { int, MySqlDecimal, mysqlTable, serial, varchar, decimal } from 'drizzle-orm/mysql-core'
 
 export const usersTable = mysqlTable('users_table', {
-  id: serial().primaryKey(),
+  userId: serial().primaryKey(),
   username: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   rep_points: int()
@@ -9,14 +9,32 @@ export const usersTable = mysqlTable('users_table', {
 
 
 export const postsTable = mysqlTable('posts_table', {
-  id: serial().primaryKey(),
+  postId: serial().primaryKey(),
   text: varchar({ length: 255 }).notNull(),
-  betId: serial().primaryKey(),
+  betId: int('bet_id').notNull().references(() => betsTable.betId, { onDelete: 'cascade' }),
 
   userId: int('user_id')
-    .notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+    .notNull().references(() => usersTable.userId, { onDelete: 'cascade' }),
+
+  userRep: int('rep_points')
+  .notNull().references(() => usersTable.rep_points, { onDelete: 'cascade' }),
+})
+
+export const betsTable = mysqlTable('bets_table', {
+  betId: serial().primaryKey(),
+  text: varchar({ length: 255 }).notNull(),
+
+  repWinAmnt: int(),
+  repLossAmnt: int(),
+
+  hitPercent: int(),
+  betLine: decimal(),
+
+  userId: int('user_id')
+    .notNull().references(() => usersTable.userId, { onDelete: 'cascade' }),
 
   userRep: int('user_id')
   .notNull().references(() => usersTable.rep_points, { onDelete: 'cascade' }),
+
 })
 
