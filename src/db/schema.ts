eef777c89e7b46b8be7e4fa32f98855c19ead74a
Marion Office <87404335+Marioffic@users.noreply.github.com>
2025-09-27 03:03:27 -0400

    added postsTable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,3 +6,17 @@ export const usersTable = mysqlTable('users_table', {
   email: varchar({ length: 255 }).notNull().unique(),
   rep_points: int()
 })
+
+
+export const postsTable = mysqlTable('posts_table', {
+  id: serial().primaryKey(),
+  text: varchar({ length: 255 }).notNull(),
+  betId: serial().primaryKey(),
+
+  userId: int('user_id')
+    .notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+
+  userRep: int('user_id')
+  .notNull().references(() => usersTable.rep_points, { onDelete: 'cascade' }),
+})
+
