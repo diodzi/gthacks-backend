a2ce835bbeaf1740eb3f25b15a6bfcf7dbb1dc12
diodzi <me@diodzi.dev>
2025-09-27 14:09:12 -0400

    crud rooms

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,48 +8,48 @@ import {
 } from 'drizzle-orm/mysql-core'
 
 export const usersTable = mysqlTable('users_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   username: varchar('username', { length: 255 }).notNull(),
   email: varchar('email', { length: 255 }).notNull().unique(),
   repPoints: int('rep_points').notNull().default(0),
 })
 
 export const betsTable = mysqlTable('bets_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   name: varchar({ length: 100 }).notNull(),
   time: datetime({ mode: 'date' }).notNull(),
   betLine: decimal('bet_line', { precision: 10, scale: 2 }).notNull(),
 })
 
 export const usersBetsTable = mysqlTable('users_bets_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
-  userId: bigint('user_id', { mode: 'bigint' })
+  id: bigint('id', { mode: 'number' }).primaryKey(),
+  userId: bigint('user_id', { mode: 'number' })
     .notNull()
     .references(() => usersTable.id, { onDelete: 'cascade' }),
-  betId: bigint('bet_id', { mode: 'bigint' })
+  betId: bigint('bet_id', { mode: 'number' })
     .notNull()
     .references(() => betsTable.id, { onDelete: 'cascade' }),
 })
 
 export const postsTable = mysqlTable('posts_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   text: varchar('text', { length: 255 }).notNull(),
-  betId: bigint('bet_id', { mode: 'bigint' })
+  betId: bigint('bet_id', { mode: 'number' })
     .notNull()
     .references(() => betsTable.id, { onDelete: 'cascade' }),
-  userId: bigint('user_id', { mode: 'bigint' })
+  userId: bigint('user_id', { mode: 'number' })
     .notNull()
     .references(() => usersTable.id, { onDelete: 'cascade' }),
 })
 
 export const gameTable = mysqlTable('game_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   title: varchar('title', { length: 255 }).notNull(),
   startTime: datetime('start_time').notNull(),
 })
 
 export const roomsTable = mysqlTable('rooms_table', {
-  id: bigint('id', { mode: 'bigint' }).primaryKey(),
+  id: bigint('id', { mode: 'number' }).primaryKey(),
   ownerId: bigint('owner_id', { mode: 'bigint' })
     .notNull()
     .references(() => usersTable.id, { onDelete: 'cascade' }),
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { drizzle } from 'drizzle-orm/mysql2'
-import { usersTable } from './db/schema.js'
+import { roomsTable, usersTable } from './db/schema.js'
 import { db } from './db/index.js'
+import { eq } from 'drizzle-orm'
 
 const app = new Hono()
 
@@ -10,10 +10,41 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/api/sqlTest', async (c) => {
+app.get('/api/users', async (c) => {
   const msg = await db.select().from(usersTable)
 
-  console.log(msg)
+  return c.json({
+    ok: true,
+    message: msg[0],
+  })
+})
+
+app.get('/api/rooms', async (c) => {
+  const msg = await db.select().from(roomsTable)
+
+  return c.json({
+    ok: true,
+    message: msg[0],
+  })
+})
+
+app.get('/api/room/:id', async (c) => {
+  const roomId = c.req.param('id')
+
+  const msg = await db
+    .select()
+    .from(roomsTable)
+    .where(eq(roomsTable.id, Number(roomId)))
+
+  return c.json({
+    ok: true,
+    message: msg[0],
+  })
+})
+
+app.post('/api/rooms/create', async (c) => {
+  const body = await c.req.json()
+  const msg = await db.insert(roomsTable).values(body).$returningId()
 
   return c.json({
     ok: true,
